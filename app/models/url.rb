b8680e5fc70b1df72f2f9bace7f0c41badd17fce
Lenin Luque <xleninx@gmail.com>
2015-09-25 17:21:53 -0300

    get social data of url

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,8 @@
 class Url < ActiveRecord::Base
   belongs_to :campaign
   validates :data, presence: true, url: { no_local: true, message: 'el formato no es correto' }
+
+  def social_count
+    SocialShares.selected data, %w(facebook google twitter) 
+  end
 end
