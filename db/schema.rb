bf7912abee6c326680289b8b181c528e0eb34c1d
Lenin Luque <xleninx@gmail.com>
2015-09-25 15:54:24 -0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925144911) do
+ActiveRecord::Schema.define(version: 20150925171151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20150925144911) do
 
   create_table "campaigns", force: :cascade do |t|
     t.string   "name"
-    t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -63,6 +62,13 @@ ActiveRecord::Schema.define(version: 20150925144911) do
 
   add_index "campaigns_users", ["campaign_id"], name: "index_campaigns_users_on_campaign_id", using: :btree
   add_index "campaigns_users", ["user_id"], name: "index_campaigns_users_on_user_id", using: :btree
+
+  create_table "urls", force: :cascade do |t|
+    t.string   "data"
+    t.integer  "campaign_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",   null: false
