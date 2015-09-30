0183e6fb66afe488e467b617adb04cb45ca7d728
Lenin Luque <xleninx@gmail.com>
2015-09-30 16:20:54 -0300

    add route for url show

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get "home/minor"
 
   resources :campaigns, only: [:index, :show]
+  resources :urls, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
