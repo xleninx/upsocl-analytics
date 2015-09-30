f14d6a87da5dd29031e1eafa636497c805f012a6
Lenin Luque <xleninx@gmail.com>
2015-09-30 16:20:37 -0300

    add new controller for url

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,11 @@
+class UrlsController < ApplicationController
+
+  def show
+    @url = Url.find(params[:id])
+    respond_to do |format|
+      format.html {}
+      format.json { render :json => @url.as_json( methods: [ :campaign, :social_count ]) }
+    end
+  end
+
+end
