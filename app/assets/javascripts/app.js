0b4f70fad7ef3e67dcf7b3b934e429f2c675049e
Lenin Luque <xleninx@gmail.com>
2015-09-30 16:11:11 -0300

    fir url for view campaign data

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -9,21 +9,15 @@ app = angular.module('upsocl',[
   'ui.bootstrap'
 ])
 
-// app.config(['$routeProvider', function($routeProvider){
-//   $routeProvider
-//   .when('/', { templateUrl: base_path + 'index.html.slim', controller: 'CampaignCtrl' } )
-//   .when('/campaigns/:campaignId', base_path + 'show.html.slim', controller: 'CampaignCtrl')
-// }])
-
 angular.module('upsocl').config(function($stateProvider) {
   $stateProvider.state('campaigns', {
     url: '/',
     templateUrl: base_path + 'index.html.slim',
     controller: 'CampaignListController'
-  }).state('viewCampaign', { //state for showing single movie
-    url: '/campaigns/:id',
+  }).state('viewCampaignUrl', { //state for showing single movie
+    url: '/campaign/urls/:id',
     templateUrl: base_path + 'show.html.slim',
-    controller: 'CampaignViewController'
+    controller: 'CampaignUrlViewController'
   })
 }).run(function($state) {
   $state.go('campaigns');
--- app/assets/javascripts/controllers/CampaignCtrl.js
+++ app/assets/javascripts/controllers/CampaignCtrl.js
@@ -1,12 +1,5 @@
-// app.controller('CampaignCtrl', function( $scope, $routeParams, campaignData ) {
-//
-//   $scope.data = campaignData.data
-//   campaignData.loadCampaings();
-//
-// });
-
 angular.module('upsocl.controllers', []).controller('CampaignListController', function($scope, $state, $window, Campaign) {
   $scope.campaigns = Campaign.query();
-}).controller('CampaignViewController', function($scope, $stateParams, Campaign ) {
-  $scope.campaign = Campaign.get({ id: $stateParams.id });
+}).controller('CampaignUrlViewController', function($scope, $stateParams, Url) {
+  $scope.url = Url.get({ id: $stateParams.id });
 })
