61a56aaf74dc1106dc7481b045e3d85ca3891013
Lenin Luque <xleninx@gmail.com>
2015-10-13 17:53:42 -0300

    add dimension date for page model

--- app/models/analytic/page.rb
+++ app/models/analytic/page.rb
@@ -1,8 +1,8 @@
 class Analytic::Page
   extend Legato::Model
 
-  metrics :pageviews, :visitors, :sessions, :avgtimeonpage, :percent_new_visits, :users
-  dimensions :page_path
+  metrics :pageviews, :visitors, :sessions, :avgtimeonpage, :users
+  dimensions :page_path, :date
 
   filter :path, &lambda {|path| matches(:page_path, path)}
 end
