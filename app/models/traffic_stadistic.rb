114f3ebc0d72bca882f2cc267d96bb925f896c7c
Lenin Luque <xleninx@gmail.com>
2015-10-13 15:30:09 -0300

    add traffic stadistics model

---
+++ app/models/traffic_stadistic.rb
@@ -0,0 +1,3 @@
+class TrafficStadistic < ActiveRecord::Base
+  belongs_to :url
+end
---
+++ db/migrate/20151013182156_create_traffic_stadistics.rb
@@ -0,0 +1,10 @@
+class CreateTrafficStadistics < ActiveRecord::Migration
+  def change
+    create_table :traffic_stadistics do |t|
+      t.belongs_to :url
+      t.string :traffic_type
+      t.integer :pageviews
+      t.timestamps null: false
+    end
+  end
+end
