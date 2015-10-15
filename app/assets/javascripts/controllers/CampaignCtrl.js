1c80afbd0036a02e3aed86e7f66c0588a1150d13
Lenin Luque <xleninx@gmail.com>
2015-10-15 15:16:17 -0300

    manage range datepicker of angular

--- app/assets/javascripts/controllers/CampaignCtrl.js
+++ app/assets/javascripts/controllers/CampaignCtrl.js
@@ -1,6 +1,13 @@
 angular.module('upsocl.controllers', []).controller('CampaignListController', function($scope, $state, $window, Campaign) {
   $scope.campaigns = Campaign.query();
 }).controller('CampaignUrlViewController', function($scope, $stateParams, Url) {
-  $scope.url = Url.get({ id: $stateParams.id });
-  show_country($stateParams.id);
+  $scope.date = { startDate: moment().subtract(7, "days"), endDate: moment() };
+  $scope.opts = run_datepicker();
+  $scope.$watch('date', function(newDate) {
+    var startDate = newDate.startDate.format('YYYY-MM-DD');
+    var endDate = newDate.endDate.format('YYYY-MM-DD');
+      $scope.url = Url.get({ id: $stateParams.id, startDate: startDate, endDate: endDate }, function(data){
+        draw_graphics($stateParams.id, data.stadistics);
+      });
+  }, false);
 })
