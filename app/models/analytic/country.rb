b0b54563a411e3f6e28ca5843a95acf440362cfa
Lenin Luque <xleninx@gmail.com>
2015-10-15 18:06:16 -0300

    add metrics for country stadistics

--- app/models/analytic/country.rb
+++ app/models/analytic/country.rb
@@ -1,7 +1,7 @@
 class Analytic::Country
   extend Legato::Model
 
-  metrics :pageviews, :visitors, :sessions
+  metrics :pageviews, :visitors, :sessions, :users, :avgtimeonpage
   dimensions :country, :page_path, :countryIsoCode, :date
 
   filter :path, &lambda {|path| matches(:page_path, path)}
