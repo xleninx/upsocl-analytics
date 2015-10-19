c2d63ca731cab68b591d3be50aa139fbe1190dc3
Lenin Luque <xleninx@gmail.com>
2015-10-19 14:56:52 -0300

    add whenever for cronjob

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+# Cron Jobs
+gem 'whenever', :require => false
+
 # gem unicorn
 gem "unicorn-rails"
 
---
+++ config/schedule.rb
@@ -0,0 +1,19 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+set :environment, "production"
+set :output, "log/cron_log.log"
+#
+every 12.hours do
+  rake "analytics:add_records[day]"
+end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
