48c3015c717dd0477b1c9354f684e907e056d65a
Lenin Luque <xleninx@gmail.com>
2015-10-23 14:20:49 -0300

    fix numbers of the view

--- app/models/dfp_stadistic.rb
+++ app/models/dfp_stadistic.rb
@@ -2,6 +2,6 @@ class DfpStadistic < ActiveRecord::Base
   belongs_to :url
   validates :date, uniqueness: { scope: [ :url, :line_id ] }
 
-  scope :totals, -> { { impressions: sum(:impressions), clicks: sum(:clicks), ctr: sum(:ctr) } }
+  scope :totals, -> { { impressions: sum(:impressions), clicks: sum(:clicks), ctr: ((sum(:clicks).to_f / sum(:impressions).to_f) * 100) } }
 
 end
