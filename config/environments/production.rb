1b572cf1603180c4a0f7012a3076eaa50c5cf2b1
Lenin Luque <xleninx@gmail.com>
2015-11-05 12:03:17 -0300

    config restore password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => ENV['EMAIL_HOST'] }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :user_name            => ENV['EMAIL_USER'],
+    :password             => ENV['EMAIL_PASS'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
 end
