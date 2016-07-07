d4331e0967f73086417483adf592b7b734c37f6b
Jhoynerk Caraballo <jhoynerk@gmail.com>
2016-07-07 19:21:19 -0400

    recibir cambio voto

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,14 +2,27 @@ class VotesController < ApplicationController
   respond_to :json
 
   def create
-    @url = Url.where(data: params[:url_path]).first
-    if(@url.nil?)
-
-    else
+    @url = Url.where(data: params[:url_path])
+    unless @url.nil?
+      @url = @url.first
       @url.votes << Vote.new(url_id: @url.id, reaction_id: params[:reaction_id])
       @url.save
+      response = @url.count_votes
     end
-    response = @url.count_votes
+    respond_to do |format|
+      format.json { render :json => response }
+    end
+  end
+
+  def change_vote
+    @url = Url.where(data: params[:url_path])
+    unless @url.nil?
+      @url = @url.first
+      @url.remove_vote(params[:before_vote])
+      @url.votes << Vote.new(url_id: @url.id, reaction_id: params[:reaction_id])
+      @url.save
+      response = @url.count_votes
+    end
     respond_to do |format|
       format.json { render :json => response }
     end
